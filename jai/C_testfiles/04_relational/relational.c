3a5f42bfd2905eb2b6b762c1a30a3a63f5a5cd9f
Aman Tuladhar <atuladhar.aman@gmail.com>
2025-02-17 15:11:29 -0600

    Add support for logical AND and OR operations in Tacky IR

--- jai/C_testfiles/04_relational/relational.c
+++ jai/C_testfiles/04_relational/relational.c
@@ -1,3 +1,23 @@
 int main(void) {
-    return 1 < 2;
+    return 1 && (1 / 0);
 }
+
+/*
+
+
+
+-- Tacky --
+    .program
+.main:
+    JumpIfZero(condition: Constant(1), label: label_cond_false.0)
+    Binary(op: DIV, src1: Constant(1), src2: Constant(0), dst: Var(tmp.0))
+    JumpIfZero(condition: Var(tmp.0), label: label_cond_false.0)
+    Copy(src: Constant(1), dst: Var(tmp.1))
+    Jump(label_logical_cmp_end.1)
+    Label(label_cond_false.0)
+    Copy(src: Constant(0), dst: Var(tmp.1))
+    Label(label_logical_cmp_end.1)
+    Return(Var(tmp.1))
+
+
+*/
