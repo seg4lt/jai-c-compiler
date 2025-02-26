99021e456aa86a8aefe3a9277dcf08b80f098812
seg4lt <12027886+seg4lt@users.noreply.github.com>
2025-02-25 20:57:30 -0600

    Lexer now supports conditional operator keywords like if,else,? and :

--- jai/c/main.c
+++ jai/c/main.c
@@ -1,6 +1,9 @@
 int main(void) {
-   int a = 2;
-   int b = 3 + a++;
-   int c = 4 + ++b;
-   return (a == 3 && b == 6 && c == 10);
+   int a = 1;
+   int b = 0;
+   if (a)
+       b = 1;
+   else if (b)
+       b = 2;
+   return b;
 }
