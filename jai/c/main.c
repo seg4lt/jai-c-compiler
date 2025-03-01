2fe4eaa372f1af866c2935d5fc1b17cc4bd0fb94
seg4lt <12027886+seg4lt@users.noreply.github.com>
2025-03-01 10:35:10 -0600

    Add variable resolution and instruction generation for new constructs in the parser and semantic analysis

--- jai/c/main.c
+++ jai/c/main.c
@@ -1,7 +1,12 @@
 int main(void) {
-    if (0)
-    label:
-        return 5;
-    goto label;
-    return 0;
+    int a = 10;
+    int b = 0;
+    if (a) {
+        int a = 1;
+        b = a;
+        goto end;
+    }
+    a = 9;
+end:
+    return (a == 10 && b == 1);
 }
