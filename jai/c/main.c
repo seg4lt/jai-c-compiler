effb82e303268ccc2986fd6290a07519822136db
Aman Tuladhar <atuladhar.aman@gmail.com>
2025-03-08 14:38:18 -0600

    Loop labeling for while

--- jai/c/main.c
+++ jai/c/main.c
@@ -1,5 +1,13 @@
 int main(void) {
     int x = 10;
-    x++;
+    while(x-- > 0) {
+        x--;
+        break;
+
+        while(x-- > 0) {
+            x--;
+            break;
+        }
+    }
     return x;
 }
