1d62c6f6814827fad82100d0b49c6d8519bf601d
Aman Tuladhar <atuladhar.aman@gmail.com>
2025-04-16 18:57:44 -0500

    Test for bitwise binary operation

--- src/Parser/tests/parser_test_003_binary_operator.zig
+++ src/Parser/tests/parser_test_003_binary_operator.zig
@@ -299,3 +299,251 @@ test "parser error: plus without second operand" {
     const result = doTest(src, "");
     try std.testing.expectError(error.ReceivedUnexpectedToken, result);
 }
+
+test "parser: bitwise and" {
+    const src =
+        \\int main(void) {
+        \\    return 42 & 42;
+        \\}
+    ;
+    const expected =
+        \\ -- AST -- 
+        \\[PROGRAM] 
+        \\|-[FN] main
+        \\|-|-[BLOCK] 
+        \\|-|-|-[RETURN] 
+        \\|-|-|-|-[BINARY] bitwise_and
+        \\|-|-|-|-|-42
+        \\|-|-|-|-|-42
+    ;
+    try doTest(src, expected);
+}
+
+test "parser: bitwise or" {
+    const src =
+        \\int main(void) {
+        \\    return 42 | 42;
+        \\}
+    ;
+    const expected =
+        \\ -- AST -- 
+        \\[PROGRAM] 
+        \\|-[FN] main
+        \\|-|-[BLOCK] 
+        \\|-|-|-[RETURN] 
+        \\|-|-|-|-[BINARY] bitwise_or
+        \\|-|-|-|-|-42
+        \\|-|-|-|-|-42
+    ;
+    try doTest(src, expected);
+}
+
+test "parser: bitwise precedence" {
+    const src =
+        \\int main(void) {
+        \\    return 80 >> 2 | 1 ^ 5 & 7 << 1;
+        \\}
+    ;
+    const expected =
+        \\ -- AST -- 
+        \\[PROGRAM] 
+        \\|-[FN] main
+        \\|-|-[BLOCK] 
+        \\|-|-|-[RETURN] 
+        \\|-|-|-|-[BINARY] bitwise_or
+        \\|-|-|-|-|-[BINARY] right_shift
+        \\|-|-|-|-|-|-80
+        \\|-|-|-|-|-|-2
+        \\|-|-|-|-|-[BINARY] bitwise_xor
+        \\|-|-|-|-|-|-1
+        \\|-|-|-|-|-|-[BINARY] bitwise_and
+        \\|-|-|-|-|-|-|-5
+        \\|-|-|-|-|-|-|-[BINARY] left_shift
+        \\|-|-|-|-|-|-|-|-7
+        \\|-|-|-|-|-|-|-|-1
+    ;
+    try doTest(src, expected);
+}
+
+test "parser: bitwise shift associativity" {
+    const src =
+        \\int main(void) {
+        \\  return 80 << 2 >> 1;
+        \\}
+    ;
+    const expected =
+        \\ -- AST -- 
+        \\[PROGRAM] 
+        \\|-[FN] main
+        \\|-|-[BLOCK] 
+        \\|-|-|-[RETURN] 
+        \\|-|-|-|-[BINARY] right_shift
+        \\|-|-|-|-|-[BINARY] left_shift
+        \\|-|-|-|-|-|-80
+        \\|-|-|-|-|-|-2
+        \\|-|-|-|-|-1
+    ;
+    try doTest(src, expected);
+}
+
+test "parser: bitwise shift associativity flip" {
+    const src =
+        \\int main(void) {
+        \\  return 80 >> 2 << 1;
+        \\}
+    ;
+    const expected =
+        \\ -- AST -- 
+        \\[PROGRAM] 
+        \\|-[FN] main
+        \\|-|-[BLOCK] 
+        \\|-|-|-[RETURN] 
+        \\|-|-|-|-[BINARY] left_shift
+        \\|-|-|-|-|-[BINARY] right_shift
+        \\|-|-|-|-|-|-80
+        \\|-|-|-|-|-|-2
+        \\|-|-|-|-|-1
+    ;
+    try doTest(src, expected);
+}
+
+test "parser: binary and shift" {
+    const src =
+        \\int main(void) {
+        \\  return 40 << 4 + 12 >> 1;
+        \\}
+    ;
+    const expected =
+        \\ -- AST -- 
+        \\[PROGRAM] 
+        \\|-[FN] main
+        \\|-|-[BLOCK] 
+        \\|-|-|-[RETURN] 
+        \\|-|-|-|-[BINARY] right_shift
+        \\|-|-|-|-|-[BINARY] left_shift
+        \\|-|-|-|-|-|-40
+        \\|-|-|-|-|-|-[BINARY] add
+        \\|-|-|-|-|-|-|-4
+        \\|-|-|-|-|-|-|-12
+        \\|-|-|-|-|-1
+    ;
+    try doTest(src, expected);
+}
+
+test "parse: shift left" {
+    const src =
+        \\int main(void) {
+        \\  return 40 << 4;
+        \\}
+    ;
+    const expected =
+        \\ -- AST -- 
+        \\[PROGRAM] 
+        \\|-[FN] main
+        \\|-|-[BLOCK] 
+        \\|-|-|-[RETURN] 
+        \\|-|-|-|-[BINARY] left_shift
+        \\|-|-|-|-|-40
+        \\|-|-|-|-|-4
+    ;
+    try doTest(src, expected);
+}
+
+test "parser: shift right" {
+    const src =
+        \\int main(void) {
+        \\  return 40 >> 4;
+        \\}
+    ;
+    const expected =
+        \\ -- AST -- 
+        \\[PROGRAM] 
+        \\|-[FN] main
+        \\|-|-[BLOCK] 
+        \\|-|-|-[RETURN] 
+        \\|-|-|-|-[BINARY] right_shift
+        \\|-|-|-|-|-40
+        \\|-|-|-|-|-4
+    ;
+    try doTest(src, expected);
+}
+
+test "parser: shift right negative" {
+    const src =
+        \\int main(void) {
+        \\  return -40 >> 4;
+        \\}
+    ;
+    const expected =
+        \\ -- AST -- 
+        \\[PROGRAM] 
+        \\|-[FN] main
+        \\|-|-[BLOCK] 
+        \\|-|-|-[RETURN] 
+        \\|-|-|-|-[BINARY] right_shift
+        \\|-|-|-|-|-[UNARY] negate
+        \\|-|-|-|-|-|-40
+        \\|-|-|-|-|-4
+    ;
+    try doTest(src, expected);
+}
+
+test "parser: complex shift operation" {
+    const src =
+        \\int main(void) {
+        \\  return (4 << (2 * 2)) + (100 >> (1 + 2)); // 64 + 12 = 76
+        \\}
+    ;
+    const expected =
+        \\ -- AST -- 
+        \\[PROGRAM] 
+        \\|-[FN] main
+        \\|-|-[BLOCK] 
+        \\|-|-|-[RETURN] 
+        \\|-|-|-|-[BINARY] add
+        \\|-|-|-|-|-[GROUP] 
+        \\|-|-|-|-|-|-[BINARY] left_shift
+        \\|-|-|-|-|-|-|-4
+        \\|-|-|-|-|-|-|-[GROUP] 
+        \\|-|-|-|-|-|-|-|-[BINARY] mul
+        \\|-|-|-|-|-|-|-|-|-2
+        \\|-|-|-|-|-|-|-|-|-2
+        \\|-|-|-|-|-[GROUP] 
+        \\|-|-|-|-|-|-[BINARY] right_shift
+        \\|-|-|-|-|-|-|-100
+        \\|-|-|-|-|-|-|-[GROUP] 
+        \\|-|-|-|-|-|-|-|-[BINARY] add
+        \\|-|-|-|-|-|-|-|-|-1
+        \\|-|-|-|-|-|-|-|-|-2
+    ;
+    try doTest(src, expected);
+}
+
+test "parser: xor" {
+    const src =
+        \\int main(void) {
+        \\  return 40 ^ 4;
+        \\}
+    ;
+    const expected =
+        \\ -- AST -- 
+        \\[PROGRAM] 
+        \\|-[FN] main
+        \\|-|-[BLOCK] 
+        \\|-|-|-[RETURN] 
+        \\|-|-|-|-[BINARY] bitwise_xor
+        \\|-|-|-|-|-40
+        \\|-|-|-|-|-4
+    ;
+    try doTest(src, expected);
+}
+
+test "parser error: invalid ||" {
+    const src =
+        \\int main(void) {
+        \\  return 40 | | 4;
+        \\}
+    ;
+    const result = doTest(src, "");
+    try std.testing.expectError(error.ReceivedUnexpectedToken, result);
+}
