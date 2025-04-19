01d0fd9318dcbcd021a053d282c43e788541ade8
seg4lt <12027886+seg4lt@users.noreply.github.com>
2025-04-19 11:25:23 -0500

    Unreachable replaced to parser error report
    make everything modules
    
    Make minor improvement to how struct are initialized
    
    Setup sema for error reporting

--- c/main.c
+++ c/main.c
@@ -1,5 +1,7 @@
 int main(void) {
-    int x = 0;
-    int y = 123;
+    int a = 10;
+    // This is a semantic error, not a parser error, because it's syntactically
+    // valid to apply an arbitrary number of postfix operations to an expression.
+    // This is important so we can later support expressions like a++[0]
+    return a++;
 }
-foo;
