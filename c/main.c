80e4849fc7b94abb8f819776bfcce2f53be9a3af
seg4lt <12027886+seg4lt@users.noreply.github.com>
2025-05-04 11:45:05 -0500

    Add error reporter to sema phase
    Fix test for lexer
    
    update parser print to writer so we can change implementation
    
    Add sample ast test
    
    primitive switch ast - but feel like I am doing more than what I should
    
    Looking at the test case, I think I am over thinking AST - I think it
    should be as easy as parse until you get end rcurly. Or if single stmt
    parse single stmt.
    
    If I were to parse it as normal as possible, then case statemnt can be
    placed anywhere I want.
    
    But need to make sure decleration doesn't exist - probably on sema phase

--- c/main.c
+++ c/main.c
@@ -1,7 +1,38 @@
+#ifdef SUPPRESS_WARNINGS
+#ifndef __clang__
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
+#pragma GCC diagnostic ignored "-Wswitch-unreachable"
+#endif
+#endif
+// a switch statement may jump into the middle of other control flow constructs
+
 int main(void) {
-    int a = 10;
-    // This is a semantic error, not a parser error, because it's syntactically
-    // valid to apply an arbitrary number of postfix operations to an expression.
-    // This is important so we can later support expressions like a++[0]
-    return a++;
+    int switch1 = 0;
+    int switch2 = 0;
+    int switch3 = 0;
+    switch(3) {
+        case 0: return 0;
+        case 1: if (0) {
+            case 3: switch1 = 1; break;
+        }
+        default: return 0;
+    }
+    switch(4) {
+        case 0: return 0;
+        if (1) {
+            return 0;
+        } else {
+            case 4: switch2 = 1; break;
+        }
+        default: return 0;
+    }
+    switch (5) {
+        for (int i = 0; i < 10; i = i + 1) {
+            switch1 = 0;
+            case 5: switch3 = 1; break;
+            default: return 0;
+        }
+    }
+
+    return (switch1 && switch2 && switch3);
 }
