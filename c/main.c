34d03d124feeeff7f290e22dc23848db2c8e465a
Aman Tuladhar <atuladhar.aman@gmail.com>
2025-05-09 23:59:28 -0500

    Add sema phase for switch case

--- c/main.c
+++ c/main.c
@@ -1,11 +1,8 @@
-#ifdef SUPPRESS_WARNINGS
-#ifndef __clang__
-#pragma GCC diagnostic ignored "-Wswitch-unreachable"
-#endif
-#endif
-
 int main(void) {
-    int a = 3;
-    int b = 0;
-    switch(1) if (1) case 1: return 10;
+    switch(3) {
+        int i = 0;
+        case 3:
+            return i;
+    }
+    return 0;
 }
