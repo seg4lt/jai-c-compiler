dd80ab103b0d465fd50b9a4b1d9bccb89e42d912
Aman Tuladhar <atuladhar.aman@gmail.com>
2025-05-11 10:35:12 -0500

    checkpoint

--- c/main.c
+++ c/main.c
@@ -1,13 +1,3 @@
 int main(void) {
-    switch(6) {
-        case 3:
-            return 200;
-        case 1:
-            return 100;
-        case 5:
-            return 111;
-        default:
-            return 123;
-    }
     return 69;
 }
