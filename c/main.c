b9f8aa2791b250336f51d27584dd92aef8d3bd43
seg4lt <12027886+seg4lt@users.noreply.github.com>
2025-05-10 22:20:05 -0500

    Update asm to use writer interface

--- c/main.c
+++ c/main.c
@@ -1,14 +1,3 @@
 int main(void) {
-    switch(611) {
-        case 3:
-            return 200;
-        case 1:
-            return 100;
-        case 5:
-        case 6:
-            return 111;
-        default:
-            return 123;
-    }
     return 69;
 }
