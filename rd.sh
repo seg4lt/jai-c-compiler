07710ebfd19d376753326612af42a4c32f8dc77a
seg4lt <12027886+seg4lt@users.noreply.github.com>
2025-06-20 17:45:10 -0500

    Basic fn decl validation

--- rd.sh
+++ rd.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+
+if [ -n "$TC" ]; then
+  echo "Running tests..."
+  docker-compose exec box bash -c "jai build.jai && tests/test_compiler ./out/jcc $1"
+  exit 0
+fi
+
+echo "Compiling ./c/main.c"
 docker-compose exec box bash -c "jai build.jai && ./out/jcc -- ./c/main.c $1"
