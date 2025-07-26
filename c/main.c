614c3888bf8e9e1b8b53dc7d72c4a2e8c310a82d
Aman Tuladhar <atuladhar.aman@gmail.com>
2025-07-26 16:53:01 -0500

    Refactor register handling to include size and update code emission
    - Replace Reg_Asm with Register_Asm struct including register and size
    - Update all code generation and emission to use Register_Asm
    - Adjust mov and reg helpers to handle register size
    - Update code emission to print correct register names based on size
    - Update function prologue/epilogue and stack handling for new register
      type

--- c/main.c
+++ c/main.c
@@ -1,16 +1,12 @@
 
-int sum(int p1, int p2) { return p1 + p2; }
-
-int foo(int p1, int p2, int p3, int p4, int p5, int p6, int p7, int p8) {
-  return p1 + p8;
+int foo(int p1, int p2, int p3, int p4, int p5, int p6, int p7, int p8,
+        int p9) {
+  return p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9;
 }
 
 int main(void) {
-  int a = 1;
-  int b = 2;
-  int c = 3;
-  int x = sum(a, b);
-
-  int y = foo(1, 2, 3, 4, 5, 6, 7, 8);
-  return x + y;
+  int x = 10;
+  int y = x + foo(1, 2, 3, 4, 5, 6, 7, 8, 9);
+  return y;
+  // return foo(1, 2, 3, 4, 5, 6, 7, 8, 9);
 }
