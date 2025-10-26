2a40bbbc74a6ad557fd911b1d293dae8e1ce4226
seg4lt <12027886+seg4lt@users.noreply.github.com>
2025-10-25 21:40:28 -0500

    Use struct inheritance thingy

--- rd.sh
+++ rd.sh
@@ -1,13 +1,27 @@
 #!/bin/sh
 
+if [ -n "$DOCKER" ]; then
+    if [ -n "$TC" ]; then
+        echo "Running tests..."
+        # --latest-only
+        # --keep-asm-on-failure
+        docker-compose exec -T box bash -c "jai-macos build.jai && tests/test_compiler ./out/jcc --verbose --failfast --verbose $1"
+        exit 0
+    fi
+
+    echo "Compiling ./c/main.c"
+    docker-compose exec -T box bash -c "jai-macos build.jai && ./out/jcc -- ./c/main.c $1"
+    exit 0
+fi
 
 if [ -n "$TC" ]; then
-  echo "Running tests..."
-  # --latest-only
-  # --keep-asm-on-failure 
-  docker-compose exec -T box bash -c "jai-linux build.jai && tests/test_compiler ./out/jcc --verbose --failfast --verbose $1"
-  exit 0
+    echo "Running tests..."
+    # --latest-only
+    # --keep-asm-on-failure
+    jai-linux build.jai && tests/test_compiler ./out/jcc --verbose --failfast --verbose $1
+    exit 0
 fi
 
 echo "Compiling ./c/main.c"
-docker-compose exec -T box bash -c "jai-linux build.jai && ./out/jcc -- ./c/main.c $1"
+jai-linux build.jai && ./out/jcc -- ./c/main.c $1
+exit 0
