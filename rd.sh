fced553e49c3eb5b9950f2f5cefff52d18750ae9
seg4lt <12027886+seg4lt@users.noreply.github.com>
2025-11-29 14:51:13 -0600

    Fix memory leaks

--- rd.sh
+++ rd.sh
@@ -5,12 +5,12 @@ if [ -n "$DOCKER" ]; then
         echo "Running tests..."
         # --latest-only
         # --keep-asm-on-failure
-        docker-compose exec -T box bash -c "jai-macos build.jai && tests/test_compiler ./out/jcc --verbose --failfast --verbose $1"
+        docker-compose exec -T box bash -c "jai-linux build.jai && tests/test_compiler ./out/jcc --verbose --failfast --verbose $1"
         exit 0
     fi
 
     echo "Compiling ./c/main.c"
-    docker-compose exec -T box bash -c "jai-macos build.jai && ./out/jcc -- ./c/main.c $1"
+    docker-compose exec -T box bash -c "jai-linux build.jai && ./out/jcc -- ./c/main.c $1"
     exit 0
 fi
 
