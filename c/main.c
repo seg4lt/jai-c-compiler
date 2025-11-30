ddfab9ce145881b34dfc3475cd8dd244b5b6454c
seg4lt <12027886+seg4lt@users.noreply.github.com>
2025-11-30 14:15:38 -0600

    Add way to debug types for expr

--- c/main.c
+++ c/main.c
@@ -1,19 +1,6 @@
-// int main (void) {
-//     int a = 100;
-//     int b = 200L;
-//     // int c = 300l;
-//     // int d = 123123123123;
-//     // long e = 1l;
-//     // long f = 2L;
-//     // long g = 11111111111;
-
-//     // long g = (int) 123;
-//     // long h = (long) 123;
-// }
-/* The result of a cast expression is not an lvalue */
-
 int main(void) {
     int i = 0;
-    i = (long) i = 10;
+    i = 10L;
+    int x = 1 ? 10L : 20;
     return 0;
 }
