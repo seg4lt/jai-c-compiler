013a8e2969127dd0f69213f475cfa8b1d1f8ecb6
seg4lt <12027886+seg4lt@users.noreply.github.com>
2025-11-29 22:14:38 -0600

    parse and sema for long type

--- c/main.c
+++ c/main.c
@@ -1,11 +1,19 @@
-#ifdef SUPPRESS_WARNINGS
-#ifdef __clang__
-#pragma clang diagnostic ignored "-Wbitwise-op-parentheses"
-#else
-#pragma GCC diagnostic ignored "-Wparentheses"
-#endif
-#endif
+// int main (void) {
+//     int a = 100;
+//     int b = 200L;
+//     // int c = 300l;
+//     // int d = 123123123123;
+//     // long e = 1l;
+//     // long f = 2L;
+//     // long g = 11111111111;
+
+//     // long g = (int) 123;
+//     // long h = (long) 123;
+// }
+/* The result of a cast expression is not an lvalue */
 
 int main(void) {
-    return 80 >> 2 | 1 ^ 5 & 7 << 1;
+    int i = 0;
+    i = (long) i = 10;
+    return 0;
 }
